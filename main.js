6b16a45d62bde3cc8c494013059b63993e089f13
death-inspires-me <nana.esseney@gmail.com>
2023-11-14 13:29:54 +0000

    Changed hello to hello guys

---
+++ main.js
@@ -0,0 +1,8 @@
+function isBoss(){
+    let isBoss = confirm("Are you the boss?");
+    if(isBoss){
+        alert("Welcome boss")
+    }else{alert("Please leave this page(This is just a test)")};
+}
+
+isBoss();
