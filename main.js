782614f2065ec57a063dcb321d7a614a31efc64c
death-inspires-me <nana.esseney@gmail.com>
2023-11-14 14:02:55 +0000

    added an image for hello world

--- main.js
+++
@@ -1,8 +0,0 @@
-function isBoss(){
-    let isBoss = confirm("Are you the boss?");
-    if(isBoss){
-        alert("Welcome boss")
-    }else{alert("Please leave this page(This is just a test)")};
-}
-
-isBoss();
